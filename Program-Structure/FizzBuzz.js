564c963d2d9babb38f1ea1c0d8ce3c370436908e
tsotne <tsotnemeskhuri@gmail.com>
2025-01-29 11:15:22 +0400

    Add second solution for FizzBuzz

--- Program-Structure/FizzBuzz.js
+++ Program-Structure/FizzBuzz.js
@@ -16,9 +16,9 @@ for (let i = 1; i <= 100; i++) {
   console.log(result || i);
 }
 
-// for (let n = 1; n <= 100; n++) {
-//   let output = "";
-//   if (n % 3 == 0) output += "Fizz";
-//   if (n % 5 == 0) output += "Buzz";
-//   console.log(output || n);
+// for (let i = 1; i <= 100; i++) {
+//   let result = "";
+//   if (i % 3 == 0) result += "Fizz";
+//   if (i % 5 == 0) result += "Buzz";
+//   console.log(result || i);
 // }
